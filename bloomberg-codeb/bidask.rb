d046eeac0a63a0525278efe7d04fc2ca811b5924
Haley Owen <haleyowen@utexas.edu>
2015-01-31 00:09:44 -0600

    creating page for biding and asking records

---
+++ bloomberg-codeb/bidask.rb
@@ -0,0 +1,8 @@
+class BidAsk
+  def initialize(type, stock, price, shares)
+    @type = type
+    @stock = stock
+    @price = price
+    @shares = shares
+  end
+end
--- bloomberg-codeb/stock.rb
+++ bloomberg-codeb/stock.rb
@@ -2,5 +2,52 @@
 require 'sinatra'
 
 get '/' do
-  'Hello, World'
+  res = ""
+  File.open("test.txt") do |f|
+    f.each_line do |line|
+      res += line
+    end
+  end
+  erb :buysell, :locals => {:res => parse(res)}
+end
+
+helpers do
+  def parse(res)
+    parseArr = res.split(" ")
+    bidaskArr = []
+    i = 0
+    while i < parseArr.length do
+      temp = BidAsk.new(parseArr[i], parseArr[i+1], parseArr[i+2], parseArr[i+3])
+      puts(temp.getType())
+      bidaskArr.push(temp)
+      i += 4
+    end
+
+    return bidaskArr
+  end
+end
+
+
+class BidAsk
+  def initialize(type, stock, price, shares)
+    @type = type
+    @stock = stock
+    @price = price
+    @shares = shares
+  end
+  def getType()
+    return @type
+  end
+  
+  def getStock()
+    return @stock
+  end
+  
+  def getPrice()
+    return @price
+  end
+
+  def getShares()
+    return @shares
+  end
 end
